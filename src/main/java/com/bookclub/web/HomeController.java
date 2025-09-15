03d731a3acf87936c2d73ef4fde2b36242d6c469
Joseph Youskievicz <jyouskievicz@my365.bellevue.edu>
2025-09-14 23:29:00 -0500

    Added HomeController, about and contact pages, and Bootstrap fragments for Assignment 1.3

---
+++ src/main/java/com/bookclub/web/HomeController.java
@@ -0,0 +1,24 @@
+package com.bookclub.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHome() {
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String showAboutUs() {
+        return "about";
+    }
+
+    @RequestMapping("/contact")
+    public String showContactUs() {
+        return "contact";
+    }
+}
+
