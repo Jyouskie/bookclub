34a396add4ad70da504ba342b68c3c20ab640d95
Joseph Youskievicz <jyouskievicz@my365.bellevue.edu>
2025-09-24 23:42:58 -0500

    Assignment 2.3: added model, Dao layer, MembookDao implementation, controller updates, and Thymeleaf views

--- src/main/java/com/bookclub/web/HomeController.java
+++ src/main/java/com/bookclub/web/HomeController.java
@@ -1,13 +1,24 @@
 package com.bookclub.web;
 
+import com.bookclub.model.Book;
+import com.bookclub.service.impl.MemBookDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String showHome() {
+    public String showHome(Model model) {
+        // Load books from DAO
+        MemBookDao booksDao = new MemBookDao();
+        List<Book> books = booksDao.list();
+        model.addAttribute("books", books);
         return "index";
     }
 
@@ -20,5 +31,14 @@ public class HomeController {
     public String showContactUs() {
         return "contact";
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getMonthlyBook(@PathVariable("id") String id, Model model) {
+        MemBookDao booksDao = new MemBookDao();
+        Book book = booksDao.find(id);
+        model.addAttribute("book", book);
+        return "monthly-books/view";
+    }
 }
+
 
